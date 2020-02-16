42c158556872ff3978f0064820a22ab98fe9afe2
Charoo <charootube@gmail.com>
2020-02-16 01:01:53 +0000

    - add function that generates random wes anderson palette - tidy up function that produces bar chart - apply function to list of song count tables

--- code/02_create_bar_charts.R
+++ code/02_create_bar_charts.R
@@ -22,7 +22,37 @@ read_song_count_df <- function(tables_folder){
   
 }
 
-wes_pal <- wes_palette("GrandBudapest2", 100, type = "continuous")
+# TODO work out how to get wesanderson to return a list of the NAMES of palettes
+wes_palettes_list <- c("BottleRocket1",
+                       "BottleRocket2",
+                       "Rushmore1",
+                       "Royal1",
+                       "Royal2",
+                       "Zissou1",
+                       "Darjeeling1",
+                       "Darjeeling2",
+                       "Chevalier1",
+                       "FantasticFox1",
+                       "Moonrise1",
+                       "Moonrise2",
+                       "Moonrise3",
+                       "Cavalcanti1",
+                       "GrandBudapest1",
+                       "GrandBudapest2",
+                       "IsleofDogs1",
+                       "IsleofDogs2")
+
+# Pick a random Wes Anderson palette 
+random_wes_pal <- function(){
+  
+  # wes_palettes is a list of all the palettes
+  # Here we sample a random palette from the list
+  pick_wes_pal <- wes_palettes_list[sample(1:length(wes_palettes_list), 1)]
+  # And then we interpolate the discrete scale so we have enough colours
+  continuous_wes_pal <- wes_palette(pick_wes_pal, 100, type = "continuous")
+  
+  return(continuous_wes_pal)
+}
 
 plot_top_songs_chart <- function(tables_folder){
   
@@ -31,13 +61,31 @@ plot_top_songs_chart <- function(tables_folder){
                filter(listens >= 90)
 
   top_songs_chart <- ggplot(data = top_songs, aes(x=reorder(song, -listens), y=listens, fill=as.factor(listens))) +
-                     scale_fill_manual(values = wes_pal) +
+                     scale_fill_manual(values = random_wes_pal()) +
                      geom_bar(stat="identity", show.legend = FALSE) +
                      theme_minimal() +
                      theme(axis.text.x = element_text(angle = 80, hjust = 1)) +
                      xlab("Song") + ylab("Number of Listens")
+  
+  # Get the download folder code, without the full file path
+  table_folder_code <- str_remove(tables_folder, "tables/")
+  
+  
+  # TODO this was copied from 01_. Find a way to avoid copying.
+  # Save bar chart table to subfolder in plots/ folder.
+  # We use Spotify's download code to name the subfolder so the file structure mirrors the raw_data/ and tables/ folder.
+  # If subfolder doesn't yet exist, we create it here.
+  if (dir.exists(paste0("plots/", table_folder_code))){
+    print("the subfolder for this download already exists")
+  } else {
+    dir.create(paste0("plots/", table_folder_code))
+  }
+  
+  ggsave(filename = "song_bar_chart",
+         plot = top_songs_chart, 
+         path = paste0("plots/", table_folder_code), 
+         device = "jpeg")
 }
 
+lapply(tables_folders_list, plot_top_songs_chart)
 
-charoo <- plot_top_songs_chart(tables_folders_list[1])
-charoo
